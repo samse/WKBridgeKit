d4c00c978dc99db119469ad6c845f69425dabe59
yslee.gm@gmail.com <>
2024-08-06 15:18:48 +0900

    bug-fix

--- WKBridgeKit/Common/WKPreferenceHelper.swift
+++ WKBridgeKit/Common/WKPreferenceHelper.swift
@@ -7,26 +7,26 @@
 
 import Foundation
 
-@objc open class WKPreferenceHelper  {
-    static open func get(_ key: String) -> String? {
+@objc open class WKPreferenceHelper: NSObject  {
+    static public func get(_ key: String) -> String? {
         if let value = UserDefaults.standard.string(forKey: key) {
             return value.data(using: .utf8)?.base64EncodedString()
         }
         return nil
     }
 
-    static open func get(_ key: String, defaultValue: String) -> String? {
+    static public func get(_ key: String, defaultValue: String) -> String? {
         if let value = UserDefaults.standard.string(forKey: key) {
             return value.data(using: .utf8)?.base64EncodedString()
         }
         return defaultValue
     }
 
-    static open func set(_ value: String, key: String) {
+    static public func set(_ value: String, key: String) {
         UserDefaults.standard.set(nullToNil(value: value), forKey: key)
     }
         
-    static open func remove(_ key: String) {
+    static public func remove(_ key: String) {
         UserDefaults.standard.removeObject(forKey: key)
     }
     
